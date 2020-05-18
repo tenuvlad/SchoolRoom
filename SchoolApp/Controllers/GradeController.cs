c6acc280667421b2cbab2f453519eced36f958a5
tennuvlad <tenuvlad@yahoo.com>
2020-05-18 14:58:42 +0300

    Update

---
+++ SchoolApp/Controllers/GradeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Servicies.Grades;
+using Servicies.Grades.Dto;
+
+namespace SchoolApp.Controllers
+{
+    public class GradeController : Controller
+    {
+        private readonly IGradeService _repo;
+
+        public GradeController(IGradeService repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet("grade/create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(GradeDto grade)
+        {
+            _repo.AddNewGrade(grade);
+            return View(grade);
+        }
+    }
+}
